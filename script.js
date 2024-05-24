4ace383543e8cd8ec7c14a6052891df67b98982c
Victor Zottmann <zottmann.victor@gmail.com>
2024-05-24 15:58:36 +1000

    add function to get computer choice

--- script.js
+++ script.js
@@ -2,7 +2,19 @@ const rockBtn = document.querySelector("#rock");
 const paperBtn = document.querySelector("#paper");
 const scissorsBtn = document.querySelector("#scissors");
 
-function getHumanSelection() {}
-function getComputerSelection() {}
+function getComputerChoice() {
+  let number = Math.floor(Math.random() * 3);
+
+  switch (number) {
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    case 2:
+      return "scissors";
+  }
+}
+
+function getHumanChoice() {}
 function playRound(humanSelection, computerSelection) {}
 function playGame() {}
