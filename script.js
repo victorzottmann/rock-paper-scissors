43a6c35b0d45a197373ae9ecc80be76b7cac1fed
Victor Zottmann <zottmann.victor@gmail.com>
2024-05-24 15:49:56 +1000

    adjustments

---
+++ script.js
@@ -0,0 +1,8 @@
+const rockBtn = document.querySelector("#rock");
+const paperBtn = document.querySelector("#paper");
+const scissorsBtn = document.querySelector("#scissors");
+
+function getHumanSelection() {}
+function getComputerSelection() {}
+function playRound(humanSelection, computerSelection) {}
+function playGame() {}
